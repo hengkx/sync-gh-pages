3473bc2118d39e0012236a278439d37b9854b275
hengkx <ycxzhkx@gmail.com>
2018-04-09 18:10:22 +0800

    remove not use code

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ async function downloadFile() {
 
 const server = serve(ghPagesDir, {
     port: config.port,
-    // ignore: ['node_modules']
 })
 
 downloadFile();
@@ -39,18 +38,3 @@ schedule.scheduleJob(config.schedule, async function () {
     console.log('downloadFile');
     await downloadFile();
 });
-
-// dl.pipe(process.stdout);
-// const app = express();
-
-// app.use(compression());
-// console.log(path.join(process.cwd(), 'gh-pages'));
-// app.use('/', express.static(path.join(process.cwd(), 'gh-pages')));
-
-// app.get('*', function (req, res) {
-//     res.sendFile(path.resolve('index.html'));
-// });
-
-// app.listen(11001, function () {
-//     console.log('Example app listening on port 11001!');
-// });
